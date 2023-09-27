e34a65385618560519d3e835c6f5f1c72dbc027d
torcuata22 <marilynjmarquez@gmail.com>
2023-09-27 11:09:34 -0400

    created port is open fx

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 import socket
 from colorama import init, Fore
 
+#colors:
+init()
+GREEN = Fore.GREEN
+RESET = Fore.RESET
+GRAY = Fore.LIGHTBLACK_EX
+
+#determine whether a port is open:
+def port_is_open(host, port):
+    """ Determine if 'host' has an open port """
+    #create socket:
+    s = socket.socket()
+
+    #connect to port, handle errors gracefully:
+    try:
+        s.connect((host, port))
+    except:
+        return False
+    else:
+        return True
